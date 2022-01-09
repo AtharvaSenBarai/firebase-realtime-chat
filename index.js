48284650712ebe10876ef3178fb340cadbde7049
Atharva Sen <atharvasenbarai@gmail.com>
2022-01-09 14:59:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyAgcMf7umnASf0vSmuKWJlVSIbe3K0tAfA",
-  authDomain: "chess-game-a8036.firebaseapp.com",
-  databaseURL: "https://chess-game-a8036-default-rtdb.firebaseio.com",
-  projectId: "chess-game-a8036",
-  storageBucket: "chess-game-a8036.appspot.com",
-  messagingSenderId: "481285790558",
-  appId: "1:481285790558:web:3c2218fcedb674f0f94dcf",
-  measurementId: "G-ZJ3RHK6632"
+    apiKey: AIzaSyAgcMf7umnASf0vSmuKWJlVSIbe3K0tAfA ,
+  authDomain: chess-game-a8036.firebaseapp.com ,
+  databaseURL: https://chess-game-a8036-default-rtdb.firebaseio.com ,
+  projectId: chess-game-a8036,
+  storageBucket: chess-game-a8036.appspot.com ,
+  messagingSenderId: 481285790558 ,
+  measurementId: G-ZJ3RHK6632 ,
+  appId: 1:481285790558:web:3c2218fcedb674f0f94dcf ,
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
