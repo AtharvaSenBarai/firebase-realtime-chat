daf365387b849318755a3b3d710185fe48bab7a9
Atharva Sen <atharvasenbarai@gmail.com>
2022-01-09 14:52:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: process.env.apiKey,
-  authDomain: process.env.authDomain,
-  databaseURL: process.env.databaseURL,
-  projectId: process.env.projectId,
-  storageBucket: process.env.storageBucket,
-  messagingSenderId: process.env.messagingSenderId,
-  appId: process.env.appId,
+    apiKey: "AIzaSyAgcMf7umnASf0vSmuKWJlVSIbe3K0tAfA",
+  authDomain: "chess-game-a8036.firebaseapp.com",
+  databaseURL: "https://chess-game-a8036-default-rtdb.firebaseio.com",
+  projectId: "chess-game-a8036",
+  storageBucket: "chess-game-a8036.appspot.com",
+  messagingSenderId: "481285790558",
+  appId: "1:481285790558:web:3c2218fcedb674f0f94dcf",
+  measurementId: "G-ZJ3RHK6632"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
